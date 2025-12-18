86d0a66dd14f5d013eaac2d0b24f8b24e02a3029
Kushal SG <167597895+Kushalsg62@users.noreply.github.com>
2025-12-18 17:41:41 +0530

    Add files via upload

---
+++ recipe-api-assignment/src/test/java/com/kushalsg/recipe_api_assignment/RecipeApiAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushalsg.recipe_api_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApiAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
